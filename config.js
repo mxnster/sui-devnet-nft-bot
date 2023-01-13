895d5de8d81b5c00d72a2717ec1934a847b4aa06
Alexander <globallll007@gmail.com>
2023-01-13 03:45:43 +0300

    init

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const config = {
+    // https://frigate-proxy.ru/?ref=8397880  - my ref :D
     proxy: '88.87.92.232:40172:9273e0164:62ee74418', // ip:port:login:password
     proxyLink: "https://frigate-proxy.ru/ru/change_ip/ddb4313cdfa208ba0841cbca7740685/91169",
     proxyPerWallet: false, // true, to rotate IP for each wallet
